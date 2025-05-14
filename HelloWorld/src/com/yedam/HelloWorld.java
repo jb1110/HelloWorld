53713dafa2a5c203a1ab910f67138809f76cbd55
JB Lee <dkdlskqn1@naver.com>
2025-05-14 10:46:53 +0900

    0514 첫번째 릴리즈

---
+++ HelloWorld/src/com/yedam/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.yedam;
+    // 자바: 컴파일(***.java -> ***.class)
+
+public class HelloWorld {
+	// main 메소드. 
+	public static void main(String[] args) {
+		for (int i = 1; i <= 3; i++)  // 밑에문장 3번출력.
+		System.out.println("Hello, World");
+	}
+}
